6087c1f0c93594847a80ee133a9fd03aa6787434
HaydenHo <Hayden.Ho23@ycdsbk12.ca>
2023-01-25 13:39:21 -0500

    code

--- src/Data/Sorter.java
+++ src/Data/Sorter.java
@@ -19,3 +19,55 @@ public Sorter() {
 }
 
 
+/**
+ * adds datapoint to arraylist and returns its values
+ * @param graphtype
+ * @param datapoint
+ * @return double stat values of stat is returned 
+ */
+public double addSort(GraphType graphtype, DataPoint datapoint) {
+    //adds value to object's arraylist, then returns value inputted back 
+    this.sortingArray.add(datapoint);
+    if(graphtype == GraphType.BYPOINTS) {
+        this.sortingBy = GraphType.BYPOINTS;
+        return datapoint.getPpg();
+    } else if (graphtype == GraphType.BYASSISTS) {
+        this.sortingBy = GraphType.BYASSISTS;
+        return datapoint.getApg();
+    } else if (graphtype == GraphType.BYREBOUNDS) {
+        this.sortingBy = GraphType.BYREBOUNDS;
+        return datapoint.getRpg();
+    } else if (graphtype == GraphType.BYWINSHARES) {
+        this.sortingBy = GraphType.BYWINSHARES;
+        return datapoint.getCareerWinShares();
+    } else {
+        return 0.0;
+    }
+    
+}
+
+/**
+ * allows sorting algorithm to get appropriate value from datapoint
+ * @param DataPoint datapoint 
+ * @return double 
+ */
+
+public double getValue(DataPoint dataPoint) {
+    //returns appropriate value depending on what the user asks for 
+    if(sortingBy == GraphType.BYPOINTS) {
+        return dataPoint.getPpg();
+    } else if(sortingBy == GraphType.BYASSISTS) {
+        return dataPoint.getApg();
+    } else if(sortingBy == GraphType.BYREBOUNDS) {
+        return dataPoint.getRpg();
+    } else if(sortingBy == GraphType.BYWINSHARES) {
+        return dataPoint.getCareerWinShares();
+    } else {
+        return 0.0;
+    }
+
+
+}
+
+
+
