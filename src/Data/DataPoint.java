9cd3728bb92de26ebaab7a02fdd326a7053eb556
HaydenHo <Hayden.Ho23@ycdsbk12.ca>
2023-01-24 22:41:25 -0500

    code

---
+++ src/Data/DataPoint.java
@@ -0,0 +1,21 @@
+package Data;
+
+public class DataPoint {
+    private int position;
+    private String athleteName;
+    private int startYear;
+    private int endYear;
+    private int matchCount;
+    private double totalMinutes;
+    private double pointsPerGame;
+    private double reboundsPerGame;
+    private double assistsPerGame;
+    private double stealsPerGame;
+    private double blocksPerGame;
+    private double fieldGoalPercent;
+    private double threePointPercent;
+    private double freeThrowPercent;
+    private double careerWinShares;
+    private double winSharesPer;
+    private String bRefTag;
+    private int addingTag;
