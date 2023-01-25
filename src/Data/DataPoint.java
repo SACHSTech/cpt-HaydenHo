79ed1cc36dac6f2579b674180036f4868df39d18
HaydenHo <Hayden.Ho23@ycdsbk12.ca>
2023-01-24 22:42:20 -0500

    code

--- src/Data/DataPoint.java
+++ src/Data/DataPoint.java
@@ -52,3 +52,40 @@ public class DataPoint {
             case 5:
                 this.totalMinutes = Double.parseDouble(value); 
                 break;
+            case 6:
+                this.pointsPerGame = Double.parseDouble(value); 
+                break;
+            case 7:
+                this.reboundsPerGame = Double.parseDouble(value); 
+                break;
+            case 8:
+                this.assistsPerGame = Double.parseDouble(value); 
+                break;
+            case 9:
+                this.stealsPerGame = Double.parseDouble(value); 
+                break;
+            case 10:
+                this.blocksPerGame = Double.parseDouble(value); 
+                break;
+            case 11:
+                this.fieldGoalPercent = Double.parseDouble(value); 
+                break;
+            case 12:
+                this.threePointPercent = Double.parseDouble(value); 
+                break;
+            case 13:
+                this.freeThrowPercent = Double.parseDouble(value); 
+                break;
+            case 14:
+                this.careerWinShares = Double.parseDouble(value);
+                break;
+            case 15:
+                this.winSharesPer = Double.parseDouble(value);
+                break;
+            case 16:
+                this.bRefTag = value;
+                break;
+        }
+
+        this.addingTag++;
+    }
