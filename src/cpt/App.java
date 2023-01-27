d84fecb3ff94054e1a1c52a60a68d2f9fb3dd02d
HaydenHo <Hayden.Ho23@ycdsbk12.ca>
2023-01-27 14:32:15 -0500

    code

---
+++ src/cpt/App.java
@@ -0,0 +1,81 @@
+package cpt;
+
+import ChartsManagement.ChartGenerator;
+import ChartsManagement.ChartValueController;
+import data.CSVScraper;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.Tab;
+
+
+public class App {
+
+ 
+/**
+ * A line chart demonstrating a CategoryAxis implementation.
+ */
+public class App extends Application {
+
+    
+
+    /**
+     * Overriding JavaFX Application Class method to run wanted script
+     */
+
+    @Override public void start(Stage primaryStage) throws Exception {
+        //creates objects to create chart/controls
+        ChartGenerator chart = new ChartGenerator();        
+        ChartValueController controller = new ChartValueController();
+        controller.addChartObject(chart);
+        
+        //creates tabs for linechart 
+        Tab lineChart = new Tab("Line Chart");
+        lineChart.setClosable(false);
+
+        //adds linechart and controls to hbox within tab
+        HBox layoutLineChart = new HBox(chart.createRankLineChart(), controller.lineControl());
+
+        //adds linechart content to tab
+        lineChart.setContent(layoutLineChart);
+
+        //creates tabs for barcharts
+        Tab barChart = new Tab("Bar Chart");
+        barChart.setClosable(false);
+
+        //adds barchart and controls to hbox within tab
+        HBox layoutBarChart = new HBox(chart.createBarChart(), controller.barControl());
+
+        barChart.setContent(layoutBarChart);
+
+        
+
+
+        //combines tab panes and puts it into stage 
+        TabPane tabpane = new TabPane(lineChart, barChart);
+
+        HBox layout = new HBox(tabpane);
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("SLAM Top 500 Charting");
+        primaryStage.show();
+
+    
+        
+    }
+ 
+    /**
+     * Java main for when running without JavaFX launcher
+     */
+    public static void main(String[] args) {
+        launch(args);
+
+       
+        
+    }
+}
